75bd9d3eb1a4a93d5c2b44b4f0107f336ad9f499
Chekorita <sergio.bustamante162ver@gmail.com>
2020-11-03 17:30:33 -0600

    Cambios en el control de las pantallas

--- Licoreria/src/licoreria/FXMLPrincipalController.java
+++ Licoreria/src/licoreria/FXMLPrincipalController.java
@@ -1,14 +1,60 @@
 package licoreria;
 
+import java.awt.event.ActionEvent;
+import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class FXMLPrincipalController implements Initializable{
-    
+    public void irProducto(MouseEvent event) throws IOException{
+        Parent root; 
+        root = FXMLLoader.load(getClass().getClassLoader().getResource("producto/ConsultarProductos.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("Inventario");
+        stage.setScene(new Scene(root, 900, 600));
+        stage.show();
+        // Hide this current window (if this is what you want)
+        ((Node)(event.getSource())).getScene().getWindow().hide(); 
+    }
+    public void irEmpleado(MouseEvent event) throws IOException{
+        Parent root; 
+        root = FXMLLoader.load(getClass().getClassLoader().getResource("producto/ConsultarEmpleados.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("Control de empleados");
+        stage.setScene(new Scene(root, 900, 600));
+        stage.show();
+        // Hide this current window (if this is what you want)
+        ((Node)(event.getSource())).getScene().getWindow().hide(); 
+    }
+    public void cerrarSesion(MouseEvent event) throws IOException{
+        System.out.println("Cerrando Sesion");
+        Parent root; 
+        root = FXMLLoader.load(getClass().getClassLoader().getResource("licoreria/IniciarSesion.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("Inicia sesion");
+        stage.setScene(new Scene(root, 500, 250));
+        stage.show();
+        // Hide this current window (if this is what you want)
+        ((Node)(event.getSource())).getScene().getWindow().hide();
+        System.out.println("Se ha cerrado sesion");
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
--- Licoreria/src/producto/FXMLDatosController.java
+++ Licoreria/src/producto/FXMLDatosController.java
@@ -1,14 +1,33 @@
 package producto;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class FXMLDatosController implements Initializable{
-    
+    public void agregar(MouseEvent event) throws IOException{
+        
+    }
+    public void cancelar(MouseEvent event) throws IOException{
+        Parent root; 
+        root = FXMLLoader.load(getClass().getClassLoader().getResource("licoreria/Principal.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("Inventario");
+        stage.setScene(new Scene(root, 900, 600));
+        stage.show();
+        // Hide this current window (if this is what you want)
+        ((Node)(event.getSource())).getScene().getWindow().hide(); 
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
