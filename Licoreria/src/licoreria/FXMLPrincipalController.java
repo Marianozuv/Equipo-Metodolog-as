c3304dc40d3b55c165a9b8304e55f26e02c9d8b7
Chekorita <sergio.bustamante162ver@gmail.com>
2020-11-04 16:34:12 -0600

    Alertas y cambios realizados

--- Licoreria/src/licoreria/FXMLPrincipalController.java
+++ Licoreria/src/licoreria/FXMLPrincipalController.java
@@ -13,7 +13,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -21,8 +23,24 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javax.swing.JOptionPane;
+import producto.ReporteProducto;
 
 public class FXMLPrincipalController implements Initializable{
+    ReporteProducto gpdf = new ReporteProducto();
+    @FXML
+    public void crearReporteInven(MouseEvent event){
+        System.out.println("He sido pulsado");
+        gpdf.crear();
+        // allow user to decide between yes and no
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "¿Desea abrir el reporte?", ButtonType.YES, ButtonType.NO);
+        // clicking X also means no
+        ButtonType result = alert.showAndWait().orElse(ButtonType.NO);
+        if (ButtonType.YES.equals(result)) {
+            gpdf.abrirPDF();
+        }
+    }
+    @FXML
     public void irProducto(MouseEvent event) throws IOException{
         Parent root; 
         root = FXMLLoader.load(getClass().getClassLoader().getResource("producto/ConsultarProductos.fxml"));
@@ -33,6 +51,7 @@ public class FXMLPrincipalController implements Initializable{
         // Hide this current window (if this is what you want)
         ((Node)(event.getSource())).getScene().getWindow().hide(); 
     }
+    @FXML
     public void irEmpleado(MouseEvent event) throws IOException{
         Parent root; 
         root = FXMLLoader.load(getClass().getClassLoader().getResource("producto/ConsultarEmpleados.fxml"));
@@ -43,6 +62,7 @@ public class FXMLPrincipalController implements Initializable{
         // Hide this current window (if this is what you want)
         ((Node)(event.getSource())).getScene().getWindow().hide(); 
     }
+    @FXML
     public void cerrarSesion(MouseEvent event) throws IOException{
         System.out.println("Cerrando Sesion");
         Parent root; 
--- Licoreria/src/producto/FXMLDatosController.java
+++ Licoreria/src/producto/FXMLDatosController.java
@@ -15,6 +15,8 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 
 public class FXMLDatosController implements Initializable{
     @FXML
@@ -25,16 +27,37 @@ public class FXMLDatosController implements Initializable{
     private TextField precioProductoTf;
     @FXML
     private TextField marcaProductoTf;
-    
+    @FXML
     public void agregar(MouseEvent event) throws IOException{
-        String nombre = nombreProductoTf.getText();
-        String marca = marcaProductoTf.getText();
-        float precio = Float.parseFloat(precioProductoTf.getText());
-        int cantidad = Integer.valueOf(cantidadProductoTf.getText());
-        CRUDProducto agre = new CRUDProducto();
-        agre.nuevoProducto(nombre, marca, precio, cantidad);
-        limpiar();
+        if(camposVacios()){
+            // allow user to decide between yes and no
+            Alert alert = new Alert(Alert.AlertType.WARNING, "Hay campos vacios", ButtonType.OK);
+            // clicking X also means no
+            ButtonType result = alert.showAndWait().orElse(ButtonType.OK);
+            if (ButtonType.OK.equals(result)) {}
+        }else{    
+            String nombre = nombreProductoTf.getText();
+            String marca = marcaProductoTf.getText();
+            float precio = Float.parseFloat(precioProductoTf.getText());
+            int cantidad = Integer.valueOf(cantidadProductoTf.getText());
+            CRUDProducto agre = new CRUDProducto();
+            // allow user to decide between yes and no
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "¿Realmente desea guardar?", ButtonType.YES, ButtonType.NO);
+            // clicking X also means no
+            ButtonType result = alert.showAndWait().orElse(ButtonType.NO);
+            if (ButtonType.YES.equals(result)) {
+                agre.nuevoProducto(nombre, marca, precio, cantidad);
+                // allow user to decide between yes and no
+                Alert aler = new Alert(Alert.AlertType.INFORMATION, "¡Registro exitoso!", ButtonType.OK);
+                // clicking X also means no
+                ButtonType resul = aler.showAndWait().orElse(ButtonType.OK);
+                if (ButtonType.OK.equals(resul)) {
+                    limpiar();
+                }
+            }
+        }
     }
+    @FXML
     public void cancelar(MouseEvent event) throws IOException{
         Parent root; 
         root = FXMLLoader.load(getClass().getClassLoader().getResource("producto/ConsultarProductos.fxml"));
@@ -54,5 +77,8 @@ public class FXMLDatosController implements Initializable{
         cantidadProductoTf.setText("");
         precioProductoTf.setText("");
         marcaProductoTf.setText("");
+    }
+    public boolean camposVacios(){
+        return (nombreProductoTf.getText().isEmpty() || cantidadProductoTf.getText().isEmpty() || precioProductoTf.getText().isEmpty() || marcaProductoTf.getText().isEmpty());
     }
 }
